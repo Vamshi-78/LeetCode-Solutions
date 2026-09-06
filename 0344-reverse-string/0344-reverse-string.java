03ed73caf83e38cfb03d7a3226ee302eac97d7ac
Vamshi Bandari <bandarivamshi9949@gmail.com>
2026-09-06 15:36:30 +0530

    Time: 0 ms (100%), Space: 48.4 MB (37.83%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,28 @@
+// class Solution {
+//     public void reverseString(char[] s) {
+//         Stack<Character> st=new Stack<>();
+//         for(char c:s){
+//             st.push(c);
+//         }
+//         int i=0;
+//         while(!st.isEmpty()){
+//             s[i++]=st.pop();
+//         }
+        
+//     }
+// }
+
 class Solution {
     public void reverseString(char[] s) {
-        Stack<Character> st=new Stack<>();
-        for(char c:s){
-            st.push(c);
+        int n=s.length;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
         }
-        int i=0;
-        while(!st.isEmpty()){
-            s[i++]=st.pop();
-        }
-        
     }
 }
