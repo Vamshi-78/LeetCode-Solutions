319b7485795e7ceb99a545e75a3b57c2c1571876
Vamshi Bandari <bandarivamshi9949@gmail.com>
2026-09-10 21:54:40 +0530

    Time: 33 ms (94.53%), Space: 53.5 MB (63.36%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,19 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a=0;
+    let b=1;
+    while(true){
+        yield a;
+        const temp=a;
+        a=b;
+        b+=temp;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
