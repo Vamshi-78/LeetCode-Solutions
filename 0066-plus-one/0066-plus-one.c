02822c74acf70ab9e9cbf263d0bd6d14806137ef
Vamshi Bandari <bandarivamshi9949@gmail.com>
2026-09-17 11:36:40 +0530

    Time: 0 ms (100%), Space: 9.9 MB (92.6%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.c
@@ -0,0 +1,20 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    for(int i=digitsSize-1;i>=0;i--){
+        if(digits[i]<9){
+            digits[i]=digits[i]+1;
+            *returnSize=digitsSize;
+            return digits;
+        }
+        digits[i]=0;
+    }
+    int *res=(int*)malloc((digitsSize+1)*sizeof(int));
+    res[0]=1;
+    for(int i=1;i<digitsSize+1;i++){
+        res[i]=0;
+    }
+    *returnSize=digitsSize+1;
+    return res;
+}
